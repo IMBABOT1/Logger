2d4d3d9a84439b8daf5378958625e8fefadcb442
Mikhail Shemyakin <zlovred12@gmail.com>
2021-01-23 17:30:43 +0300

    temp

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    private static final Logger LOGGER = LogManager.getLogger("ru.geekbrains");
+
+    public static void main(String[] args) {
+        LOGGER.debug("Debug");
+        LOGGER.info("Info");
+        LOGGER.warn("Warn");
+        LOGGER.error("Error");
+        LOGGER.fatal("Fatal");
+        LOGGER.info("INFO");
+    }
+}
